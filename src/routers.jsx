1ebb86bded6194d35325d890edb8a5ad3263c077
Sergio Francisco <spinheiro.lf@gmail.com>
2022-03-14 18:21:45 -0300

    fazendo as rotas necessárias

---
+++ src/routers.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search/index';
+import Album from './pages/Album/index';
+import Favorites from './pages/Favorites/index';
+import Profile from './pages/Profile/index';
+import ProfileEdit from './pages/Profile/Edit/index';
+import NotFound from './pages/NotFound/index';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={ Login } />
+    <Route exact path="/search" component={ Search } />
+    <Route exact path="/album/:id" component={ Album } />
+    <Route exact path="/favorites" component={ Favorites } />
+    <Route exact path="/profile" component={ Profile } />
+    <Route exact path="/profile/edit" component={ ProfileEdit } />
+    <Route path="*" component={ NotFound } />
+  </Switch>
+);
+
+export default Routes;
