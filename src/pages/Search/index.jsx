6992813d029db5f367f5462f54c7d67a750a1725
Sergio Francisco <spinheiro.lf@gmail.com>
2022-03-15 10:47:35 -0300

    fazendo o form de pesquisa

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -2,9 +2,46 @@ import React, { Component } from 'react';
 import Header from '../../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      // search: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  handleChange({ target }) {
+    const minLength = 2;
+    console.log(target.value.length);
+    if (target.value.length >= minLength) {
+      this.setState({ isButtonDisabled: false });
+    } else {
+      this.setState({ isButtonDisabled: true });
+    }
+    // this.setState({ search: target.value });
+  }
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
-      <div data-testid="page-search"><Header /></div>
+      <div data-testid="page-search">
+        <Header />
+        <section>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </section>
+      </div>
     );
   }
 }
